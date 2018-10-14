148f605db64ee1adc13ac84ee2687691fbdded32
Tanguy-bdjk <tanguybondjokwe@gmail.com>
2018-10-14 14:21:55 +0200

    js

---
+++ script.js
@@ -0,0 +1,12 @@
+function vert() {
+  document.getElementById('text').className='green';
+
+}
+
+function rouge() {
+  document.getElementById('text').className='red';
+}
+
+function bleu() {
+  document.getElementById('text').className='blue';
+}
